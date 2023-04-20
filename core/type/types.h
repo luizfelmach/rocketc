84b2537ac53392b674b03ab29553d9d64e16a6c6
Luiz Felipe Machado <luizfelmach@gmail.com>
2023-04-20 14:44:01 -0300

    refactor

--- core/type/types.h
+++ core/type/types.h
@@ -1,6 +1,12 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+#define or  ||
+#define and &&
+#define not !
+
+typedef enum { true = 1, false = 0 } boolean;
+
 int    *i32(int x);
 float  *f32(float x);
 double *f64(double x);
