ba3b0f1473ab1a02d5c22319cdd0b57f062bd866
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-04-30 12:19:50 -0300

    add double linked list node

--- containers/list/node.c
+++ containers/list/node.c
@@ -11,3 +11,15 @@ Node_List node_list_new(void *value, Node_List next) {
 void node_list_destroy(Node_List node) {
     free(node);
 }
+
+Node_DList node_dlist_new(void *value, Node_DList prev, Node_DList next) {
+    Node_DList node = calloc(1, sizeof(struct _node_dlist));
+    node->value     = value;
+    node->prev      = prev;
+    node->next      = next;
+    return node;
+}
+
+void node_dlist_destroy(Node_DList node) {
+    free(node);
+}
--- containers/list/node.h
+++ containers/list/node.h
@@ -6,7 +6,16 @@ typedef struct _node_list {
     struct _node_list *next;
 } *Node_List;
 
+typedef struct _node_dlist {
+    void               *value;
+    struct _node_dlist *prev;
+    struct _node_dlist *next;
+} *Node_DList;
+
 Node_List node_list_new(void *value, Node_List next);
 void      node_list_destroy(Node_List node);
+
+Node_DList node_dlist_new(void *value, Node_DList prev, Node_DList next);
+void       node_dlist_destroy(Node_DList node);
 
 #endif
