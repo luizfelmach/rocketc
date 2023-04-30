a02eb07ce8b9725ff53dfc70098a89e93e7aa405
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-04-30 17:05:34 -0300

    add vector test dict people

--- containers/dict/tests/people.c
+++ containers/dict/tests/people.c
@@ -7,19 +7,27 @@ Dict people(char *name, int age, float height) {
         pair(str("age"), i32(age)),
         pair(str("height"), f32(height))
     );
+    // clang-format on
 
     return people;
 }
 
 int main() {
-
-    smart Dict p1 = people("Luiz Felipe Machado", 85, 1.83);
+    smart Vector peoples = vector();
+    smart Dict   p1      = people("Luiz Felipe Machado", 85, 1.83);
 
     print("p1: {}\n\n", p1);
     print("name: {}\n", dict_get(p1, str("name")));
     print("age: {}\n", dict_get(p1, str("age")));
     print("height: {}\n", dict_get(p1, str("height")));
     print("undefined attr: {}\n", dict_get(p1, str("test")));
+
+    int i;
+    for (i = 0; i < 10; i++) {
+        vector_push(peoples, people("p", i, 19.1));
+    }
+
+    print("peoples: {}\n", peoples);
 
     return 0;
 }
